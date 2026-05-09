d5a9a5d48af09939aa6e6f17c782913ded837673
net <l@itinet>
2026-05-09 16:54:28 +0300

    ok

--- cleanbuild2.sh
+++ cleanbuild2.sh
@@ -5,6 +5,8 @@ git clean -fdx
 rm -rf prisma/migrations
 npm install
 npx prisma generate
-npx prisma migrate dev # add-user-auth
+npx prisma migrate dev --name add-user-auth
+# npx prisma migrate dev --name add_image
 npm run db:seed
 npm run dev
+
