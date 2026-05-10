19984ee9bdba7f183af22b331edad79f5219707b
net <l@itinet>
2026-05-10 14:18:39 +0300

    added frontend page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const authRouter = require("./routes/auth");
-const postsRouter = require("./routes/posts");
+const questionsRouter = require("./routes/posts");
 const prisma = require("./lib/prisma");
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use("/api/auth", authRouter);
-app.use("/api/posts", postsRouter);
+app.use("/api/posts", questionsRouter);
 
 app.use((req, res) => {
     res.status(404).json({msg: "Not found"});
